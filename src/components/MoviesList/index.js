f3c652735326b10f1850937e72c7f4667a9f4e0f
jamilelima <jamilesousalima@gmail.com>
2019-01-10 22:00:42 -0300

    Implementing MoviesList layout and logic for render an MovieItem

---
+++ src/components/MoviesList/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import MovieItem from './components/MovieItem';
+
+const movies = {
+  movie1: {
+    movieId: 1,
+    original_title: 'Wonder Woman',
+    poster: null,
+  },
+  movie2: {
+    movieId: 1,
+    original_title: 'Wonder Woman',
+    poster: null,
+  },
+  movie3: {
+    movieId: 1,
+    original_title: 'Wonder Woman',
+    poster: null,
+  },
+  movie4: {
+    movieId: 1,
+    original_title: 'Wonder Woman',
+    poster: null,
+  },
+  movie5: {
+    movieId: 1,
+    original_title: 'Wonder Woman',
+    poster: null,
+  },
+  movie6: {
+    movieId: 1,
+    original_title: 'Wonder Woman',
+    poster: null,
+  },
+  movie7: {
+    movieId: 1,
+    original_title: 'Wonder Woman',
+    poster: null,
+  },
+  movie8: {
+    movieId: 1,
+    original_title: 'Wonder Woman',
+    poster: null,
+  },
+}
+
+class MoviesList extends Component {
+
+  navigateToMovieDetails = () => {
+    // @TODO: Implementar navegação para Movie Details
+  }
+
+  render() {
+    return (
+      <div className="movies-list-container">
+        {
+          Object.keys(movies).map(key => (
+            <MovieItem
+              key={key}
+              movieDetails={movies[key]}
+              onClick={this.navigateToMovieDetails}
+            />
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+export default MoviesList;
