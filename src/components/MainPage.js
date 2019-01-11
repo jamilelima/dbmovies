313e4fd0aabf7c5ce8abef089bcfa9e512a5ef3a
jamilelima <jamilesousalima@gmail.com>
2019-01-11 20:33:23 -0300

    Init integration with API of the movies db

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,7 +4,24 @@ import SearchMovieBar from './SearchMovieBar';
 import Footer from './Footer';
 import MovieDetailsCard from './MovieDetailsCard';
 
+
+const API_KEY = '?api_key=f0ec5dea593ad4b500ce7d88c8dede74';
+const API_KEY_ALT = '&api_key=f0ec5dea593ad4b500ce7d88c8dede74';
+const URL_SEARCH = 'https://api.themoviedb.org/3/search/movie?query=';
+const URL_DETAIL = 'https://api.themoviedb.org/3/movie/';
+
+
+
 class MainPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      suggestions: [],
+    }
+  }
+
 
 
   render() {
