03571281502c6d91359a1e1e1c781b4eb21fa2f5
jamilelima <jamilesousalima@gmail.com>
2019-01-10 22:49:15 -0300

    Adjust style for movie list item

--- src/components/MoviesList/components/MovieItem/index.js
+++ src/components/MoviesList/components/MovieItem/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 const MovieItem = ({ original_title, poster }) => (
   <div className="movie-item-container">
     <img src="https://pbs.twimg.com/media/DwG19SEUYAEzmwk.jpg:small" alt="Poster Image" className="movie-poster" />
-    <p className="movie-title">{original_title}</p>
+    <div className="movie-title-container">
+      <h3 className="movie-title">Wonder Woman</h3>
+    </div>
   </div>
+  // <Link className="movie-item-container" to={'/'}>
+  //   <img src="https://pbs.twimg.com/media/DwG19SEUYAEzmwk.jpg:small" alt="Poster Image" className="movie-poster" />
+  //   <div className="">
+  //     <h3 className="">Wonder Woman</h3>
+  //   </div>
+  // </Link >
 )
 
 
