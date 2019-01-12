b0c2e4905871a716df0993f9b9d664f7657b457d
jamilelima <jamilesousalima@gmail.com>
2019-01-12 10:04:49 -0300

    Change Header component to stateful component and add Autosuggest lib

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,128 @@
-import React from 'react';
+// CORE
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+
+// HELPERS
+import Autosuggest from 'react-autosuggest';
+import { URL_SEARCH, API_KEY_ALT, URL_MOVIES_SEARCH } from '../../utils';
+
+// STYLES
 import '../../styles/header.scss';
 
-const Header = () => (
-  <nav>
-    <div class="logo">
-      <h1>MOVIES DB</h1>
-    </div>
-    <div class="search">
-      <input class="search-input" placeholder="Search" />
-      <button class="fa fa-search search-button"></button>
-    </div>
-  </nav>
-)
+
+class Header extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      suggestions: [],
+    }
+  }
+
+  onChange = ({ newValue }) => {
+    this.setState({
+      value: newValue,
+    })
+  }
+
+  handleKeyDown = (event) => {
+    const { value } = this.state;
+    if (event.key === 'Enter') {
+      return this.handleSubmit(value);
+    }
+  }
+
+  handleSubmit = (searchText) => {
+    const { dispatch } = this.props;
+    dispatch(push(`/search/${searchText}`));
+    this.setState({ value: '' });
+  }
+
+  getSuggestionValue = (suggestion) => {
+    return suggestion.title;
+  }
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    const suggestionValue = value.trim(); // The trim() method removes whitespace from both sides of a string.
+    if (suggestionValue.length > 0) {
+      let url = `${URL_MOVIES_SEARCH}${suggestionValue}${API_KEY_ALT}`;
+      fetch(url).then(response => response.json())
+        .then(json => json.results)
+        .then(data => {
+          const results = data.map(movie => {
+            const movieDetails = {};
+            movieDetails.id = movie.id
+            movieDetails.title = movie.title
+            movieDetails.img = movie.poster_path
+            movieDetails.year = (movie.release_date === '') ? "0000" : movie.release_date.substring(0, 4)
+            return movieDetails
+          });
+          this.setState({
+            suggestions: results
+          });
+        }).catch(error => console.log(error, " - Exception"));
+    } else {
+      this.setState({
+        suggestions: []
+      })
+    }
+  }
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  renderSuggestion = (suggestion) => (
+    <a>
+      <div className="search-result-text">
+        {suggestion.title}
+      </div>
+    </a>
+  );
+
+  onSuggestionSelected = (event, { suggestion, method }) => {
+    if (method === 'enter')
+      event.preventDefault();
+    this.props.dispatch(push('/movie/' + suggestion.id));
+    this.setState({ value: '' });
+  };
+
+  render() {
+
+    const { value, suggestions } = this.state;
+
+    const inputProps = {
+      value,
+      onChange: this.onChange,
+      onKeyPress: this.handleKeyDown,
+      placeholder: 'Search Movie Title...'
+    };
+
+    return (
+      <nav>
+        <div class="logo">
+          <h1>MOVIES DB</h1>
+        </div>
+        <div class="search">
+          <input class="search-input" placeholder="Search" />
+          <button class="fa fa-search search-button"></button>
+          <Autosuggest
+            suggestions={suggestions}
+            onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+            onSuggestionSelected={this.onSuggestionSelected}
+            onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+            getSuggestionValue={this.getSuggestionValue}
+            renderSuggestion={this.renderSuggestion}
+            inputProps={inputProps} />
+        </div>
+      </nav>
+    );
+  }
+}
 
 
-export default Header;
+export default connect()(Header);
