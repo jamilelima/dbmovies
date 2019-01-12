b390d83397e29d040e9839bf00ce296dab2b3d34
jamilelima <jamilesousalima@gmail.com>
2019-01-12 13:08:02 -0300

    Add url movie poster to get image poster

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,4 @@ export const URL_MOVIES_SEARCH = 'https://api.themoviedb.org/3/search/movie?quer
 export const URL_MOVIE_DETAIL = 'https://api.themoviedb.org/3/movie/';
 export const API_KEY = '?api_key=f0ec5dea593ad4b500ce7d88c8dede74';
 export const API_KEY_ALT = '&api_key=f0ec5dea593ad4b500ce7d88c8dede74';
+export const URl_MOVIE_POSTER = 'https://image.tmdb.org/t/p/';
