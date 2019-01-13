17aa2db2d441f453b2ae63f96cada176c4c24eeb
jamilelima <jamilesousalima@gmail.com>
2019-01-13 10:54:48 -0300

    Change footer indentation

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import '../../styles/footer.scss'
 const Footer = () => (
   <div className="footer-container">
-    <p>Made with <a className="code-icon" href="https://github.com/jamilelima/dbmovies" target="_blank" rel="noopener noreferrer"><i className="fas fa-code"></i></a> and <i className="fas fa-mug-hot"></i> by <a className="github-profile" href="https://github.com/jamilelima" target="_blank" rel="noopener noreferrer"> Jamile Lima</a>
+    <p>
+      Made with <a className="code-icon" href="https://github.com/jamilelima/dbmovies" target="_blank" rel="noopener noreferrer"><i className="fas fa-code"></i></a> and <i className="fas fa-mug-hot"></i> by <a className="github-profile" href="https://github.com/jamilelima" target="_blank" rel="noopener noreferrer"> Jamile Lima</a>
     </p>
   </div>
 )
