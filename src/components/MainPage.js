ac45c7149efac5f895da22985531370303bbef76
Jamile Lima <jamilelima@protonmail.com>
2019-01-18 23:37:56 -0300

    Create Movie item ui

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,39 +1,53 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import MovieDetailsCard from './MovieDetailsCard';
-import Footer from './Footer';
-import { connect } from 'react-redux';
-import { getMovieDetails } from '../actions';
+import React, { Component } from "react";
+import Header from "./Header";
+// import MovieDetailsCard from "./MovieDetailsCard";
+import Footer from "./Footer";
+import { connect } from "react-redux";
+import { getMovieDetails } from "../actions";
+import MovieItem from "./MoviesList/components/MovieItem";
 
-const favoriteMovies = [297802, 299536, 383498, 363088, 284054, 141052, 297762, 263115, 284052, 271110, 345920, 300668];
+const favoriteMovies = [
+  297802,
+  299536,
+  383498,
+  363088,
+  284054,
+  141052,
+  297762,
+  263115,
+  284052,
+  271110,
+  345920,
+  300668
+];
 class MainPage extends Component {
-
   componentDidMount() {
     const { dispatch } = this.props;
     dispatch(getMovieDetails(this.randomNumbers()));
   }
 
   randomNumbers = () => {
-    const number = favoriteMovies[Math.floor(Math.random() * favoriteMovies.length)];
+    const number =
+      favoriteMovies[Math.floor(Math.random() * favoriteMovies.length)];
     return number;
-  }
-
+  };
 
   render() {
     const { movieData } = this.props;
     return (
       <React.Fragment>
         <Header />
-        <MovieDetailsCard movieData={movieData} />
+        {/* <MovieDetailsCard movieData={movieData} /> */}
+        <MovieItem movieData={movieData} />
         <Footer />
       </React.Fragment>
-    )
+    );
   }
 }
 
 function mapStateToProps(state) {
   const { movieData } = state;
-  return { movieData }
+  return { movieData };
 }
 
 export default connect(mapStateToProps)(MainPage);
---
+++ src/components/MoviesList/components/MovieItem/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import "../../../../styles/movie_item.scss";
+
+import { URl_MOVIE_POSTER, IMG_SIZE } from "../../../../utils";
+
+class MovieItem extends Component {
+  getFormattedReleaseDate = () => {
+    const { release_date } = this.props.movieData.movie;
+    if (release_date !== undefined) {
+      const formattedReleaseDate = release_date
+        .substring(0, 10)
+        .split("-")
+        .reverse()
+        .join("/");
+      return formattedReleaseDate;
+    } else {
+      return null;
+    }
+  };
+
+  render() {
+    const {
+      title,
+      tagline,
+      overview,
+      poster_path,
+      imdb_id,
+      homepage
+    } = this.props.movieData.movie;
+
+    // const formattedReleaseDate = this.getFormattedReleaseDate();
+
+    return (
+      <div className="container">
+        <div className="card-content">
+          <img src={`${URl_MOVIE_POSTER}${IMG_SIZE}${poster_path}`} alt="" />
+          <div className="card-info">
+            <div className="header-container">
+              <h1 className="title-text">{title}</h1>
+            </div>
+            <div className="movie-details">
+              <p>{overview}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieItem;
