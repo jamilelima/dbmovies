18f0902af701d45e39c86cd23fc10d8a4e14e215
Jamile Lima <jamilelima@protonmail.com>
2019-01-19 18:28:02 -0300

    Refactoring route manager

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import React, { Component } from 'react';
-import MainPage from './components/MainPage';
+/* Core */
+import React, { Component } from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
-class App extends Component {
+/* Redux */
+import { Provider, connect } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+/* Reducers */
+import dbMoviesApp from "./reducers";
+
+/* Components */
+import MovieDetailsCard from "./components/MovieDetailsCard";
+import MainPage from "./components/MainPage";
+
+let store = createStore(dbMoviesApp, applyMiddleware(thunk));
+
+class RootContainerComponent extends Component {
   render() {
     return (
-      <div className="App">
-        <MainPage />
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route
+            path="/movie/:movieId"
+            render={routeProps => (
+              <MovieDetailsCard
+                {...routeProps}
+                movieData={this.props.movieData}
+              />
+            )}
+          />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    movieData: state.movieData
+  };
+};
+
+let RootContainer = connect(
+  mapStateToProps,
+  null
+)(RootContainerComponent);
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <RootContainer />
+      </Provider>
+    );
+  }
+}
