f8ec827578b65ba2bc0c7db3906f2293bcf84076
Jamile Lima <jamilelima@protonmail.com>
2019-01-19 21:26:02 -0300

    Add items to show on details, creating methods to format some data

--- src/components/MovieDetailsCard/index.js
+++ src/components/MovieDetailsCard/index.js
@@ -1,13 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "../../styles/card.scss";
 import {
   URl_MOVIE_POSTER,
   IMG_SIZE,
   getFormattedReleaseDate,
-  getFormattedGenre
+  getFormattedGenre,
+  URL_YOUTUBE
 } from "../../utils";
 import { connect } from "react-redux";
 import { getMovieDetails } from "../../actions";
+import { formatMoney } from "../../utils";
+import CircleInfo from "../CircleInfo";
+import Header from "../Header";
+import "../../styles/body.scss";
 
 class MovieDetailsCard extends Component {
   componentDidMount() {
@@ -16,77 +21,148 @@ class MovieDetailsCard extends Component {
     dispatch(getMovieDetails(movieId));
   }
 
+  translateMovieStatus = status => {
+    let statusPtBr;
+    switch (status) {
+      case "Released":
+        statusPtBr = "Lançado";
+        break;
+      case "Post Production":
+        statusPtBr = "Pós produção";
+        break;
+      default:
+        statusPtBr = "Não encontrado";
+        break;
+    }
+    return statusPtBr;
+  };
+
+  translateMovieLanguage = language => {
+    let languageName;
+    switch (language) {
+      case "pt":
+        languageName = "Português";
+        break;
+      case "es":
+        languageName = "Espanhol";
+        break;
+      case "hi":
+        languageName = "Hindi";
+        break;
+      case "en":
+        languageName = "Inglês";
+        break;
+      default:
+        languageName = "Não informado";
+        break;
+    }
+    return languageName;
+  };
+
+  convertAndFormatRuntime = runtime => {
+    const hours = runtime / 60;
+    const roundHours = Math.floor(hours);
+    const minutes = (hours - roundHours) * 60;
+    const roundMinutes = Math.round(minutes);
+    return `${roundHours}h ${roundMinutes}min`;
+  };
+
+  formatMovieTrailerUrl = movieVideoObj => {
+    if (movieVideoObj !== undefined) {
+      if (movieVideoObj.results[0]) {
+        const youtubeKey = movieVideoObj.results[0].key;
+        const trailerUrl = `${URL_YOUTUBE}${youtubeKey}`;
+        return trailerUrl;
+      }
+    } else {
+      return null;
+    }
+  };
+
   render() {
     const {
       title,
-      tagline,
       overview,
       poster_path,
       vote_average,
       runtime,
-      imdb_id,
-      homepage,
+      budget,
+      revenue,
+      status,
       release_date,
-      genres
+      genres,
+      original_language,
+      videos
     } = this.props.movieData.movie;
 
     const formattedReleaseDate = getFormattedReleaseDate(release_date);
     const formattedGenre = getFormattedGenre(genres);
+    const statusName = this.translateMovieStatus(status);
+    const movieLanguage = this.translateMovieLanguage(original_language);
+    const movieRuntime = this.convertAndFormatRuntime(runtime);
+    const formattedBudget = formatMoney.format(budget);
+    const formattedRevenue = formatMoney.format(revenue);
+    const profit = formatMoney.format(revenue - budget);
+    const movieTrailerUrl = this.formatMovieTrailerUrl(videos);
 
     return (
-      <div className="movie-details-container">
-        <div className="movie-title-container">
-          <h1 className="movie-title-text">{title}</h1>
-          <p className="formatted-release-date">{formattedReleaseDate}</p>
-        </div>
-        <div className="info-container">
-          <div className="overview-container">
-            <h3 className="overview-title">Sinopse</h3>
-            <p className="movie-details-overview-text">{overview}</p>
+      <Fragment>
+        <Header />
+        <div className="movie-details-container">
+          <div className="movie-title-container">
+            <h1 className="movie-title-text">{title}</h1>
+            <p className="formatted-release-date">{formattedReleaseDate}</p>
+          </div>
+          <div className="info-container">
+            <div className="overview-container">
+              <h3 className="overview-title">Sinopse</h3>
+              <p className="movie-details-overview-text">{overview}</p>
 
-            <h3 className="overview-title">Informações</h3>
-            <div className="info-items-container">
-              <div className="info-item">
-                <h6>Situação</h6>
-                <span>Lançado</span>
+              <h3 className="overview-title">Informações</h3>
+              <div className="info-items-container">
+                <div className="info-item">
+                  <h6>Situação</h6>
+                  <span>{statusName}</span>
+                </div>
+                <div className="info-item">
+                  <h6>Idioma</h6>
+                  <span>{movieLanguage}</span>
+                </div>
+                <div className="info-item">
+                  <h6>Duração</h6>
+                  <span>{movieRuntime}</span>
+                </div>
+                <div className="info-item">
+                  <h6>Orçamento</h6>
+                  <span>{formattedBudget}</span>
+                </div>
+                <div className="info-item">
+                  <h6>Receita</h6>
+                  <span>{formattedRevenue}</span>
+                </div>
+                <div className="info-item">
+                  <h6>Lucro</h6>
+                  <span>{profit}</span>
+                </div>
               </div>
-              <div className="info-item">
-                <h6>Idioma</h6>
-                <span>Inglês</span>
-              </div>
-              <div className="info-item">
-                <h6>Duração</h6>
-                <span>2h 10min</span>
-              </div>
-              <div className="info-item">
-                <h6>Orçamento</h6>
-                <span>$180.000.000,00</span>
-              </div>
-              <div className="info-item">
-                <h6>Receita</h6>
-                <span>$853.977.000,00</span>
-              </div>
-              <div className="info-item">
-                <h6>Lucro</h6>
-                <span>$673.977.000,00</span>
+              <div className="movie-info-footer">
+                <ul className="movie-details-genres">{formattedGenre}</ul>
+                <CircleInfo avarage={vote_average} nameOfClass="big_circle" />
               </div>
             </div>
-            <ul className="movie-details-genres">{formattedGenre}</ul>
-          </div>
-          <div>
             <img src={`${URl_MOVIE_POSTER}${IMG_SIZE}${poster_path}`} alt="" />
           </div>
+          <iframe
+            width="auto"
+            height="720"
+            src={movieTrailerUrl}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Movie trailer"
+          />
         </div>
-        <iframe
-          width="auto"
-          height="720"
-          src="https://www.youtube.com/embed/GX33bIOA5aA"
-          frameborder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          title="Movie trailer"
-        />
-      </div>
+      </Fragment>
     );
   }
 }
