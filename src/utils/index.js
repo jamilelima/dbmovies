2051a3976e620d823dc15ce0ca01993d95d601f8
Jamile Lima <jamilelima@protonmail.com>
2019-01-19 21:26:41 -0300

    Add method to format money and create const with youtube url

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,6 +6,7 @@ export const URL_MOVIE_DETAIL = "https://api.themoviedb.org/3/movie/";
 export const API_KEY = "?api_key=f0ec5dea593ad4b500ce7d88c8dede74";
 export const API_KEY_ALT = "&api_key=f0ec5dea593ad4b500ce7d88c8dede74";
 export const URl_MOVIE_POSTER = "https://image.tmdb.org/t/p/";
+export const URL_YOUTUBE = "https://www.youtube.com/embed/";
 export const IMG_SIZE = "w342/";
 
 // METHODS
@@ -36,3 +37,9 @@ export const getFormattedGenre = genres => {
     return null;
   }
 };
+
+export const formatMoney = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  minimumFractionDigits: 2
+});
